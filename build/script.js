ae18ff665d9ed304fc07856dfd9d6f5e533ea65c
Eric Flatt <ericflatt@Erics-MacBook-Pro-3.local>
2020-05-20 00:41:24 -0400

    commit

--- build/script.js
+++ build/script.js
@@ -1,28 +1,6 @@
-/**
- * Flickity Transformer
- *
- * https://github.com/elcontraption/flickity-transformer
- *
- * By Darin Reid: elcontraption.com
- * 
- * Find Metafizzy's Flickity here: https://flickity.metafizzy.co/
- *
- * What is this?
- * Flickity Transformer hooks into version 2's new `scroll` event to apply css transforms to slides.
- *
- * How do I play with this?
- * - Create a transform in the `transforms` array. Each transform must have two properties: `name` (the transform function name), and an array of `stops`.
- * - Each stop is an array with two values: x position in pixels relative to the home position of your carousel, and the transform value to apply at that position.
- *
- * See documentation at https://github.com/elcontraption/flickity-transformer
- *
- */
-
-
-
 const flkty = new Flickity('.Slider', {
     setGallerySize: false,
-    pageDots: false,
+    pageDots: true,
   })
   
   const transformer = new FlickityTransformer(flkty, [
@@ -51,7 +29,6 @@ const flkty = new Flickity('.Slider', {
       ]
     },
     {
-      // Declare perspective here, before rotateY. At least two stops are required, hence the duplication.
       name: 'perspective',
       stops: [
         [0, 600],
