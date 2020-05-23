ea06a19dc0afd913c9687c8ba8b1c6bc110d7488
Eric Flatt <ericflatt@Erics-MacBook-Pro-3.local>
2020-05-22 21:50:20 -0400

    lets get this bread

--- source/js/app.js
+++ source/js/app.js
@@ -1,69 +1,24 @@
-/**
- * Flickity Transformer
- *
- * https://github.com/elcontraption/flickity-transformer
- *
- * By Darin Reid: elcontraption.com
- * 
- * Find Metafizzy's Flickity here: https://flickity.metafizzy.co/
- *
- * What is this?
- * Flickity Transformer hooks into version 2's new `scroll` event to apply css transforms to slides.
- *
- * How do I play with this?
- * - Create a transform in the `transforms` array. Each transform must have two properties: `name` (the transform function name), and an array of `stops`.
- * - Each stop is an array with two values: x position in pixels relative to the home position of your carousel, and the transform value to apply at that position.
- *
- * See documentation at https://github.com/elcontraption/flickity-transformer
- *
- */
+var modal = document.getElementById("myModal");
 
+// Get the button that opens the modal
+var btn = document.getElementById("modalTrigger");
 
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
 
-const flkty = new Flickity('.Slider', {
-    setGallerySize: false,
-    pageDots: false
-  })
-  
-  const transformer = new FlickityTransformer(flkty, [
-    {
-      name: 'scale',
-      stops: [
-        [-300, 0.5],
-        [0, 0.8],
-        [300, 0.5]
-      ]
-    },
-    {
-      name: 'translateY',
-      stops: [
-        [-1000, 500],
-        [0, 0],
-        [1000, 500]
-      ]
-    },
-    {
-      name: 'rotate',
-      stops: [
-        [-300, -30],
-        [0, 0],
-        [300, 30]
-      ]
-    },
-    {
-      // Declare perspective here, before rotateY. At least two stops are required, hence the duplication.
-      name: 'perspective',
-      stops: [
-        [0, 600],
-        [1, 600]
-      ]
-    },
-    {
-      name: 'rotateY',
-      stops: [
-        [-300, 45],
-        [0, 0],
-        [300, -45]
-      ]
-    }
-  ])
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
